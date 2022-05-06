c217297b03621c9c762b368c6821bb3581ccca39
zaj-e <carbonelmarcelo@gmail.com>
2022-05-06 01:56:55 -0500

    Serv n sd

---
+++ FirelessApi/Extensions/ModeStateExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FirelessApi.Extensions;
+
+public static class ModeStateExtensions
+{
+    public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+    {
+        return dictionary.SelectMany(m => m.Value.Errors)
+            .Select(m => m.ErrorMessage)
+            .ToList();
+    }
+}
