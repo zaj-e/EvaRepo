9b92718470d22cfb615f43d036daa09d3282cad9
zaj-e <carbonelmarcelo@gmail.com>
2022-05-06 01:59:44 -0500

    Mapping and swagger

---
+++ FirelessApi/Mapping/Mappings.cs
@@ -0,0 +1,17 @@
+﻿using FirelessApi.Controllers.DataObjects;
+using FirelessApi.Domain;
+using AutoMapper;
+
+namespace FirelessApi.Mapping;
+
+public class Mappings : Profile
+{
+    public Mappings()
+    {
+        CreateMap<NewUser, User>();
+        CreateMap<User, User>();
+        CreateMap<Data, Data>();
+        CreateMap<Alert, Alert>();
+        CreateMap<Region, Region>();
+    }
+}
---
+++ FirelessApi/Swagger/SwaggerExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace FirelessApi.Swagger;
+
+public static class SwaggerExtensions
+{
+    public static void AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "OffiRent API",
+                    Version = "v1",
+                    Description = "OffiRent RESTful API"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. <BR/>
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      <BR/>Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+                // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                // c.IncludeXmlComments(xmlPath);
+                c.EnableAnnotations();
+            });
+        }
+        
+        public static void UseCustomSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "api-docs/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/api-docs/v1/swagger.json", "OffiRent API V1");
+                c.RoutePrefix = "api-docs/v1";
+            });
+        }
+}
