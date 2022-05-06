24259ca68c5385ffcf9c220e40a585d3a399ed86
zaj-e <carbonelmarcelo@gmail.com>
2022-05-06 01:57:20 -0500

    persistence

---
+++ FirelessApi/Persistence/FirelessDbContext.cs
@@ -0,0 +1,53 @@
+﻿using FirelessApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirelessApi.Persistence;
+
+public class FirelessDbContext : DbContext
+{
+    public DbSet<Alert> Alerts { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Data> Data { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public FirelessDbContext(DbContextOptions<FirelessDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Alert>(venue =>
+        {
+            venue.HasKey(v => v.Id);
+            venue.Property(v => v.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+        });
+            
+        builder.Entity<Region>(venue =>
+        {
+            venue.HasKey(v => v.Id);
+            venue.Property(v => v.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+        });
+
+        builder.Entity<Data>(venue =>
+        {
+            venue.HasKey(v => v.Id);
+            venue.Property(v => v.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+        });
+
+        builder.Entity<User>(venue =>
+        {
+            venue.HasKey(v => v.Id);
+            venue.Property(v => v.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+        });
+    }
+}
---
+++ FirelessApi/Persistence/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using FirelessApi.Domain.Repository;
+
+namespace FirelessApi.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly FirelessDbContext _context;
+
+    public UnitOfWork(FirelessDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
